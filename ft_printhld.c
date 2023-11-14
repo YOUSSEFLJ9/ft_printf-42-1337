fa87b98663199be5034aa3da44e63bdc1d2859ba
YOUSSEFLJ9 <youssefmomen@outlook.com>
2023-11-14 16:26:31 +0100

    fix edge of holder

--- ft_printhld.c
+++ ft_printhld.c
@@ -6,7 +6,7 @@
 /*   By: ymomen <ymomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:43:34 by ymomen            #+#    #+#             */
-/*   Updated: 2023/11/13 23:14:40 by ymomen           ###   ########.fr       */
+/*   Updated: 2023/11/14 15:12:54 by ymomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 void	ft_printhld(const char *format, char *holder, int *hldsize, int *count)
 {
-	int	g_errcheck;
-
 	if (*hldsize == LIMT || *format == '%')
 	{
 		*count += write(STDUT, holder, *hldsize);
-		if (*format + 1 == '\0')
-			g_errcheck = -1;
+		if (*hldsize == LIMT)
+			*count += write(STDUT, format, 1);
 		*hldsize = 0;
 		ft_bzero(holder, LIMT);
 	}
